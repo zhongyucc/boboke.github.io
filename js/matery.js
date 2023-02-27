586f7e79995218b95a9a45833c996b29beabc375
Bobo <997010078@qq.com>
2023-02-27 13:39:14 +0800

    Site updated: 2023-02-27 13:39:06

--- js/matery.js
+++ js/matery.js
@@ -2,19 +2,19 @@ $(function () {
     /**
      * 添加文章卡片hover效果.
      */
-    let articleCardHover = function () {
-        let animateClass = 'animated pulse';
-        $('article .article').hover(function () {
-            $(this).addClass(animateClass);
-        }, function () {
-            $(this).removeClass(animateClass);
-        });
-    };
-    articleCardHover();
+    // let articleCardHover = function () {
+    //     let animateClass = 'animated pulse';
+    //     $('article .article').hover(function () {
+    //         $(this).addClass(animateClass);
+    //     }, function () {
+    //         $(this).removeClass(animateClass);
+    //     });
+    // };
+    // articleCardHover();
 
     /*菜单切换*/
     $('.sidenav').sidenav();
-
+    
     /* 修复文章卡片 div 的宽度. */
     let fixPostCardWidth = function (srcId, targetId) {
         let srcDiv = $('#' + srcId);
@@ -118,6 +118,7 @@ $(function () {
 
     $('.modal').modal();
 
+
     /*回到顶部*/
     $('#backTop').click(function () {
         $('body,html').animate({scrollTop: 0}, 400);
@@ -127,6 +128,7 @@ $(function () {
     /*监听滚动条位置*/
     let $nav = $('#headNav');
     let $backTop = $('.top-scroll');
+    let $waveline = $('.wavelineheader');
     // 当页面处于文章中部的时候刷新页面，因为此时无滚动，所以需要判断位置,给导航加上绿色。
     showOrHideNavBg($(window).scrollTop());
     $(window).scroll(function () {
@@ -140,12 +142,15 @@ $(function () {
         if (position < showPosition) {
             $nav.addClass('nav-transparent');
             $backTop.slideUp(300);
+            $waveline.removeClass('wavelineshow');
+            // $('#sma').fadeOut(); //浅色暗色按钮切换模式的根据窗口位置进出动画
         } else {
             $nav.removeClass('nav-transparent');
             $backTop.slideDown(300);
+            $waveline.addClass('wavelineshow');
+            // $('#sma').fadeIn(); //浅色暗色按钮切换模式的根据窗口位置进出动画
         }
     }
-
     	
 	$(".nav-menu>li").hover(function(){
 		$(this).children('ul').stop(true,true).show();
@@ -171,19 +176,65 @@ $(function () {
     $('.tooltipped').tooltip();
 });
 
-//黑夜模式提醒开启功能
-setTimeout(function () {
-    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
-        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
-        M.toast({ html: toastHTML })
-    }
-}, 2200);
+// 模仿知乎卡片样式的超链接
+$(function () {
+    NProgress.done();
+    //增加 LinkCard 的 js 代码
+    var LinkCards=document.getElementsByClassName('LinkCard');
+    if(LinkCards.length != 0){
+	var LinkCard=LinkCards[0];
+	var link=LinkCard.href;
+	var title=LinkCard.innerText;
+	LinkCard.innerHTML="<style type=text/css>.LinkCard,.LinkCard:hover{text-decoration:none;border:none!important;color:inherit!important}.LinkCard{position:relative;display:block;margin:1em auto;width:390px;box-sizing:border-box;border-radius:12px;max-width:100%;overflow:hidden;color:inherit;text-decoration:none}.ztext{word-break:break-word;line-height:1.6}.LinkCard-backdrop{position:absolute;top:0;left:0;right:0;bottom:0;background-repeat:no-repeat;-webkit-filter:blur(20px);filter:blur(20px);background-size:cover;background-position:center}.LinkCard,.LinkCard:hover{text-decoration:none;border:none!important;color:inherit!important}.LinkCard-content{position:relative;display:flex;align-items:center;justify-content:space-between;padding:12px;border-radius:inherit;background-color:rgba(246,246,246,0.88)}.LinkCard-text{overflow:hidden}.LinkCard-title{display:-webkit-box;-webkit-line-clamp:2;overflow:hidden;text-overflow:ellipsis;max-height:calc(16px * 1.25 * 2);font-size:16px;font-weight:500;line-height:1.25;color:#1a1a1a}.LinkCard-meta{display:flex;margin-top:4px;font-size:14px;line-height:20px;color:#999;white-space:nowrap}.LinkCard-imageCell{margin-left:8px;border-radius:6px}.LinkCard-image{display:block;width:60px;height:auto;border-radius:inherit}</style><span class=LinkCard-backdrop style=background-image:url(https://zhstatic.zhihu.com/assets/zhihu/editor/zhihu-card-default.svg)></span><span class=LinkCard-content><span class=LinkCard-text><span class=LinkCard-title>"+title+"</span><span class=LinkCard-meta><span style=display:inline-flex;align-items:center>​<svg class="+"'Zi Zi--InsertLink'"+" fill=currentColor viewBox="+"'0 0 24 24'"+" width=17 height=17><path d="+"'M6.77 17.23c-.905-.904-.94-2.333-.08-3.193l3.059-3.06-1.192-1.19-3.059 3.058c-1.489 1.489-1.427 3.954.138 5.519s4.03 1.627 5.519.138l3.059-3.059-1.192-1.192-3.059 3.06c-.86.86-2.289.824-3.193-.08zm3.016-8.673l1.192 1.192 3.059-3.06c.86-.86 2.289-.824 3.193.08.905.905.94 2.334.08 3.194l-3.059 3.06 1.192 1.19 3.059-3.058c1.489-1.489 1.427-3.954-.138-5.519s-4.03-1.627-5.519-.138L9.786 8.557zm-1.023 6.68c.33.33.863.343 1.177.029l5.34-5.34c.314-.314.3-.846-.03-1.176-.33-.33-.862-.344-1.176-.03l-5.34 5.34c-.314.314-.3.846.03 1.177z'"+" fill-rule=evenodd></path></svg></span>"+link+"</span></span><span class=LinkCard-imageCell><img class=LinkCard-image alt=图标 src=https://site-1258928558.cos.ap-guangzhou.myqcloud.com/linkcard.png></span></span>";
 
-//黑夜模式判断
-if (localStorage.getItem('isDark') === '1') {
-    document.body.classList.add('DarkMode');
-    $('#sum-moon-icon').addClass("fa-sun").removeClass('fa-moon')
-} else {
-    document.body.classList.remove('DarkMode');
-    $('#sum-moon-icon').removeClass("fa-sun").addClass('fa-moon')
-}
+	for (var i = LinkCards.length - 1; i >= 1; i--) {
+		LinkCard=LinkCards[i];
+		title=LinkCard.innerText;
+		link=LinkCard.href;
+		LinkCard.innerHTML="<span class=LinkCard-backdrop style=background-image:url(https://zhstatic.zhihu.com/assets/zhihu/editor/zhihu-card-default.svg)></span><span class=LinkCard-content><span class=LinkCard-text><span class=LinkCard-title>"+title+"</span><span class=LinkCard-meta><span style=display:inline-flex;align-items:center>​<svg class="+"'Zi Zi--InsertLink'"+" fill=currentColor viewBox="+"'0 0 24 24'"+" width=17 height=17><path d="+"'M6.77 17.23c-.905-.904-.94-2.333-.08-3.193l3.059-3.06-1.192-1.19-3.059 3.058c-1.489 1.489-1.427 3.954.138 5.519s4.03 1.627 5.519.138l3.059-3.059-1.192-1.192-3.059 3.06c-.86.86-2.289.824-3.193-.08zm3.016-8.673l1.192 1.192 3.059-3.06c.86-.86 2.289-.824 3.193.08.905.905.94 2.334.08 3.194l-3.059 3.06 1.192 1.19 3.059-3.058c1.489-1.489 1.427-3.954-.138-5.519s-4.03-1.627-5.519-.138L9.786 8.557zm-1.023 6.68c.33.33.863.343 1.177.029l5.34-5.34c.314-.314.3-.846-.03-1.176-.33-.33-.862-.344-1.176-.03l-5.34 5.34c-.314.314-.3.846.03 1.177z'"+" fill-rule=evenodd></path></svg></span>"+link+"</span></span><span class=LinkCard-imageCell><img class=LinkCard-image alt=图标 src=https://site-1258928558.cos.ap-guangzhou.myqcloud.com/linkcard.png></span></span>";
+		}
+	}
+});
+
+// 网站全局深色模式设置开始
+function switchNightMode() {
+    var body = document.body;
+    if(body.classList.contains('dark')){
+      document.body.classList.remove('dark');
+      localStorage.setItem('dark','0');
+      $('#nightMode').removeClass("fa-lightbulb-o").addClass("fa-moon-o");
+      return;
+    } else {
+      document.body.classList.add('dark');
+      localStorage.setItem('dark','1');
+      $('#nightMode').removeClass("fa-moon-o").addClass("fa-lightbulb-o");
+      return;
+    }
+};
+// 网站全局深色模式设置结束
+
+// 网站主题切换模式开始
+// 中国节日按钮切换
+function switchSpringThemes() {
+    var body = document.body;
+    var weiletree = document.getElementById('weletree');
+    if(body.classList.contains('themechris')){
+        document.body.classList.remove('themechris');
+        localStorage.setItem('themechris','0');
+        document.body.classList.add('themespring');
+        localStorage.setItem('themespring','1');
+        return;
+    }
+    if(body.classList.contains('themespring')){
+    document.body.classList.remove('themespring');
+    localStorage.setItem('themespring','0');
+    return;
+    } else {
+    document.body.classList.add('themespring');
+    localStorage.setItem('themespring','1');
+    return;
+    }
+};
+// 网站主题切换模式结束
+ 
+console.log("恭喜你喜提彩蛋！欢迎常来本博客逛逛~");
